252bf265dfdf5f16c742f9db3a557b0012145538
David García Cancio <davidgaca@edu.xunta.gal>
2025-02-05 11:33:24 +0100

    C#18 Reorganizar de nuevo tema 7

--- FPADISTANCIA/APUNTES/TEMA-07/dwes_tema_07/P07_03_01_03_ejercicio/ajax.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require(__DIR__ . '/defs.php');
-
-// Procesar la solicitud
-if($jaxon->canProcessRequest())  $jaxon->processRequest();
--- FPADISTANCIA/APUNTES/TEMA-07/dwes_tema_07/P07_03_01_03_ejercicio/cerrar.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-
-if(isset($_SESSION['usu'])) unset($_SESSION['usu']);
-
-header('Location:login.php');

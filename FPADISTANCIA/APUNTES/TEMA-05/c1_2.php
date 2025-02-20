b7711e4ed9af78050d41ddebf0ab6f158fdccf5d
David García Cancio <davidgaca@edu.xunta.gal>
2025-02-20 11:47:44 +0100

    C#34 Añadir ejemplo solución Tarea 7

--- FPADISTANCIA/APUNTES/TEMA-05/c1_2.php
+++ FPADISTANCIA/APUNTES/TEMA-05/c1_2.php
@@ -7,9 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS BootStrap -->
     <link rel="stylesheet"
-    href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifj
-    h" crossorigin="anonymous">
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>Ejemplo Tema 4</title>
 </head>
 
@@ -26,4 +25,5 @@
         </div>
     </div>
 </body>
+
 </html>
